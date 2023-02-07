91b48746d3a053bba8b1ec7c3c305b34cff4cb19
Zach Kelling <z@zeekay.io>
2023-02-06 20:31:32 -0600

    Use proxy for API calls in production and dev

---
+++ src/setupProxy.js
@@ -0,0 +1,34 @@
+const { createProxyMiddleware } = require('http-proxy-middleware')
+
+module.exports = function(app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'https://beta.api.uniswap.org/v1/graphql',
+      changeOrigin: true,
+      pathRewrite: {
+        '^/api': '/', // remove base path
+      },
+    })
+  )
+  app.use(
+    '/api-beta',
+    createProxyMiddleware({
+      target: 'https://temp.api.uniswap.org/v1',
+      changeOrigin: true,
+      pathRewrite: {
+        '^/api-beta': '/', // remove base path
+      },
+    })
+  )
+  app.use(
+    '/cmc',
+    createProxyMiddleware({
+      target: 'https://api.coinmarketcap.com/data-api/v3/uniswap/all.json',
+      changeOrigin: true,
+      pathRewrite: {
+        '^/cmc': '/', // remove base path
+      },
+    })
+  )
+}
