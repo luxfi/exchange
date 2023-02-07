b3bf50463d8327647bf75c712a90e89403229f99
Zach Kelling <z@zeekay.io>
2023-02-07 01:46:15 -0600

    Fix local dev proxy

--- api/beta.js
+++ api/beta.js
@@ -1,23 +1,11 @@
-// Create a proxy to redirect requests of the "/api/*" path to "https://example.org".
-//
-// Examples:
-// GET /api/hello → GET https://example.org/hello
-// POST /api/test?color=red → POST https://example.org/test?color=red
-//
-// Additionally, the proxy will:
-// - Add an "x-added" header
-// - Remove the "x-removed" header
-// From the proxied response.
-//
-// You can/should update the proxy to suit your needs.
-// See https://github.com/chimurai/http-proxy-middleware for more details.
 const { createProxyMiddleware } = require('http-proxy-middleware')
 
-const apiProxy = createProxyMiddleware({
+const apiProxy = createProxyMiddleware('/api/beta', {
   target: 'https://temp.api.uniswap.org/v1',
   changeOrigin: true,
+  includePrefix: false,
   pathRewrite: {
-    '^/api/beta': '' // strip "/api" from the URL
+    '^/api/beta': '/' // strip "/api" from the URL
   },
   onProxyReq(proxyReq) {
     proxyReq.setHeader('origin', 'http://localhost:3000')
@@ -33,7 +21,9 @@ const apiProxy = createProxyMiddleware({
   logLevel: 'debug'
 })
 
-// Expose the proxy on the "/api/*" endpoint.
-export default function (req, res) {
+function proxy(req, res) {
   return apiProxy(req, res)
 }
+proxy.middleware = apiProxy
+
+module.exports = proxy
