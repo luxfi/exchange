6fee726f68828c0d445376677a26e4df16ac336e
Zach Kelling <z@zeekay.io>
2023-02-07 12:12:02 -0600

    Update env variables

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,6 +1,6 @@
 module.exports = function(app) {
   // Order matters here
-  app.use('/api/beta', require('../api/beta').middleware)
-  app.use('/api/cmc', require('../api/cmc').middleware)
-  app.use('/api', require('../api/index').middleware)
+  app.use('/api',  require('../api/index').middleware)
+  app.use('/beta', require('../api/beta').middleware)
+  app.use('/cmc',  require('../api/cmc').middleware)
 }
