a401e062f983ca1466869c334c417d39fe68c129
Zach Kelling <z@zeekay.io>
2023-02-07 01:46:15 -0600

    Fix local dev proxy

--- api/index.js
+++ api/index.js
@@ -1,29 +1,18 @@
-// Create a proxy to redirect requests of the "/api/*" path to "https://example.org".
-//
-// Examples:
-// GET /api/hello → GET https://example.org/hello
-// POST /api/test?color=red → POST https://example.org/test?color=red
-//
-// Additionally, the proxy will:
-// - Add an "x-added" header
-// - Remove the "x-removed" header
-// From the proxied response.
-//
-// You can/should update the proxy to suit your needs.
-// See https://github.com/chimurai/http-proxy-middleware for more details.
 const { createProxyMiddleware } = require('http-proxy-middleware')
 
-const apiProxy = createProxyMiddleware({
+const apiProxy = createProxyMiddleware('/api', {
   target: 'https://beta.api.uniswap.org/v1/graphql',
   changeOrigin: true,
+  includePrefix: false,
   pathRewrite: {
-    '^/api': '' // strip "/api" from the URL
+    '^/api': '/' // strip "/api" from the URL
   },
   onProxyReq(proxyReq) {
     proxyReq.setHeader('origin', 'http://localhost:3000')
     proxyReq.setHeader('Content-Type', 'application/json')
     proxyReq.setHeader('Referer', 'http://localhost:3000/')
     proxyReq.setHeader('Cookie', 'AMP_0000000000=JTdCJTIyb3B0T3V0JTIyJTNBZmFsc2UlMkMlMjJkZXZpY2VJZCUyMiUzQSUyMmY3Y2U2YjZjLTk2YTEtNDI0Ny1hNTdjLWVjY2NmOWUyMjNlZiUyMiUyQyUyMmxhc3RFdmVudFRpbWUlMjIlM0ExNjc1NzUwNDEyMzMyJTJDJTIyc2Vzc2lvbklkJTIyJTNBMTY3NTc1MDQxMDA3OCU3RA==; AMP_MKTG_0000000000=JTdCJTdE; _hp2_id.2120523489=%7B%22userId%22%3A%221901858558924873%22%2C%22pageviewId%22%3A%226086101989268534%22%2C%22sessionId%22%3A%227284413905776725%22%2C%22identity%22%3Anull%2C%22trackerVersion%22%3A%224.0%22%7D; _ga_M5131VBRN3=GS1.1.1675324128.4.1.1675326878.60.0.0; _ga=GA1.1.2101045042.1675291292; _fbp=fb.0.1675291683098.113117798; _ga_KDP9B6W4H8=GS1.1.1675642019.2.0.1675642019.0.0.0')
+    proxyReq.setHeader('User-Agent', 'User-Agent Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/110.0')
   },
   onProxyRes(proxyRes) {
     proxyRes.headers['Cache-Control'] = 's-maxage=1, stale-while-revalidate'
@@ -31,7 +20,9 @@ const apiProxy = createProxyMiddleware({
   logLevel: 'debug',
 })
 
-// Expose the proxy on the "/api/*" endpoint.
-export default function (req, res) {
+function proxy(req, res) {
   return apiProxy(req, res)
 }
+proxy.middleware = apiProxy
+
+module.exports = proxy
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,34 +1,6 @@
-const { createProxyMiddleware } = require('http-proxy-middleware')
-
 module.exports = function(app) {
-  app.use(
-    '/api',
-    createProxyMiddleware({
-      target: 'https://beta.api.uniswap.org/v1/graphql',
-      changeOrigin: true,
-      pathRewrite: {
-        '^/api': '/', // remove base path
-      },
-    })
-  )
-  app.use(
-    '/api/beta',
-    createProxyMiddleware({
-      target: 'https://temp.api.uniswap.org/v1',
-      changeOrigin: true,
-      pathRewrite: {
-        '^/api/beta': '/', // remove base path
-      },
-    })
-  )
-  app.use(
-    '/api/cmc',
-    createProxyMiddleware({
-      target: 'https://api.coinmarketcap.com/data-api/v3/uniswap/all.json',
-      changeOrigin: true,
-      pathRewrite: {
-        '^/api/cmc': '/', // remove base path
-      },
-    })
-  )
+  // Order matters here
+  app.use('/api/beta', require('../api/beta').middleware)
+  app.use('/api/cmc', require('../api/cmc').middleware)
+  app.use('/api', require('../api/index').middleware)
 }
