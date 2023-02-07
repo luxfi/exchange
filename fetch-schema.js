6dbd9bd65ad4e5db24beb14be6cd1aa9ecc30146
Zach Kelling <z@zeekay.io>
2023-02-06 21:21:30 -0600

    Update production

--- fetch-schema.js
+++ fetch-schema.js
@@ -11,7 +11,7 @@ function fetchSchema(url, outputFile) {
     `get-graphql-schema --h Origin=https://app.uniswap.org ${url} | tee ${outputFile}.temp`,
     (error, stdout, stderr) => {
       if (error || stderr) {
-        console.log(`Failed to fetch schema from ${url}`)
+        console.log(`Failed to fetch schema from ${url}: ${stderr}`)
       } else if (stdout) {
         exec(`mv ${outputFile}.temp ${outputFile}`)
       }
@@ -20,4 +20,4 @@ function fetchSchema(url, outputFile) {
 }
 
 fetchSchema(process.env.THE_GRAPH_SCHEMA_ENDPOINT, thegraphConfig.schema)
-fetchSchema(process.env.REACT_APP_AWS_API_ENDPOINT, dataConfig.schema)
+fetchSchema(process.env.REACT_APP_PUBLIC_API_ENDPOINT, dataConfig.schema)
