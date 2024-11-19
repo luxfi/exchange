a2fba68517a6a3632c8a8d91ac8e846be65e9f5b
foxier25 <danibaker0388@gmail.com>
2024-11-19 14:37:30 -0600

    update graph chart

--- src/pages/TokenDetails/index.tsx
+++ src/pages/TokenDetails/index.tsx
@@ -15,7 +15,7 @@ import { Chain, TokenQuery, Currency, TokenQueryData } from 'graphql/data/Token'
 import { TokenPriceQuery } from 'graphql/data/TokenPrice'
 
 const GetTokenInfo = gql`
-query GetTokenInfo($tokenAddress: String!) {
+query GetTokenInfo($tokenAddress: String!, $days: Int, $hours: Int) {
   bundles(first: 10) {
     ethPriceUSD
   }
@@ -29,9 +29,17 @@ query GetTokenInfo($tokenAddress: String!) {
     derivedETH
 
     # Get daily data for more detailed metrics
-    tokenDayData(first: 365, orderBy: date, orderDirection: asc) {
+    tokenDayData(first: $days, orderBy: date, orderDirection: desc) {
       id
       date
+      priceUSD  # The price of the token in USD for the day
+      totalValueLockedUSD
+      volumeUSD
+    }
+
+    tokenHourData(first: $hours, orderBy: periodStartUnix, orderDirection: desc) {
+      id
+      periodStartUnix
       priceUSD  # The price of the token in USD for the day
       totalValueLockedUSD
       volumeUSD
@@ -57,11 +65,29 @@ query GetTokenInfo($tokenAddress: String!) {
 
 export const pageTimePeriodAtom = atomWithStorage<TimePeriod>('tokenDetailsTimePeriod', TimePeriod.DAY)
 
+function getQueryParams(timePeriod:any) {
+  switch(timePeriod) {
+    case 0:
+      return [2, 2];
+    case 1:
+      return [2, 24];
+    case 2:
+      return [2, 168];
+    case 3:
+      return [30, 2];
+    case 4:
+      return [365, 2];
+  }
+  return [2, 2];
+}
+
+
 export default function TokenDetailsPage() {
   const { tokenAddress, chainName } = useParams<{ tokenAddress: string; chainName?: string }>()
   const chain = validateUrlChainParam(chainName)
   const isNative = tokenAddress === NATIVE_CHAIN_ID
   const [timePeriod, setTimePeriod] = useAtom(pageTimePeriodAtom)
+  const [totalDays, totalHours] = getQueryParams(timePeriod);
   const [address, duration] = useMemo(
     /* tokenAddress will always be defined in the path for for this page to render, but useParams will always
       return optional arguments; nullish coalescing operator is present here to appease typechecker */
@@ -82,11 +108,12 @@ export default function TokenDetailsPage() {
       duration,
     },
   })
-
   const { data: luxData, loading: luxLoading } = useQuery(GetTokenInfo, {
     client: luxClient,
     variables: {
       tokenAddress: address,
+      days: totalDays,
+      hours: totalHours,
     },
   })
   if (luxLoading) {
@@ -152,28 +179,29 @@ const transformedTokenDetail: TokenQuery = tokenPriceQuery??{
 };
 const ethPriceUSD = luxData?.bundles[0]?.ethPriceUSD;
 const ethPrice = luxData?.token?.derivedETH;
+const tokenDayData = luxData?.token?.tokenDayData.filter((data: any) => parseFloat(data.priceUSD) !== 0).map((data: any) => ({
+  id: `VGltZXN0YW1wZWRBbW91bnQ6MV8x${data.date}_VVNE`, // Encoded version of the timestamped amount
+  timestamp: data.date,
+  value: parseFloat(data.priceUSD),
+}))
+const tokenHourData = luxData?.token?.tokenHourData.filter((data: any) => parseFloat(data.priceUSD) !== 0).map((data: any) => ({
+  id: `VGltZXN0YW1wZWRBbW91bnQ6MV8x${data.periodStartUnix}_VVNE`, // Encoded version of the timestamped amount
+  timestamp: data.periodStartUnix,
+  value: parseFloat(data.priceUSD),
+}))
 
 const transformedTokenPriceHistory:TokenPriceQuery = {
   token: {
-    __typename: "Token",
     id: "VG9rZW46RVRIRVJFVU1fMHhhMGI4Njk5MWM2MjE4YjM2YzFkMTlkNGEyZTllYjBjZTM2MDZlYjQ4", // Encoded version of the token ID
     address: luxData?.token?.id,
     chain: Chain.Lux,
     market: {
-      __typename: "TokenMarket",
       id: "VG9rZW5NYXJrZXQ6RVRIRVJFVU1fMHhhMGI4Njk5MWM2MjE4YjM2YzFkMTlkNGEyZTllYjBjZTM2MDZlYjQ4X1VTRA==", // Encoded ID for the market
       price: {
-        __typename: "Amount",
         id: "QW1vdW50OjFfVVNE", // Encoded amount ID
         value: parseFloat(ethPriceUSD) * parseFloat(ethPrice),
       },
-      priceHistory: luxData?.token?.tokenDayData.filter((data: any) => parseFloat(data.priceUSD) !== 0)
-      .map((data: any) => ({
-        __typename: "TimestampedAmount",
-        id: `VGltZXN0YW1wZWRBbW91bnQ6MV8x${data.date}_VVNE`, // Encoded version of the timestamped amount
-        timestamp: data.date,
-        value: parseFloat(data.priceUSD),
-      })),
+      priceHistory: timePeriod < 3 ? tokenHourData : tokenDayData,
     },
   },
 };
