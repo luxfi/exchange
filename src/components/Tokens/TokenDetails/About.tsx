9163862fab6091a98a8dd532d484b35bccc509c4
foxier25 <danibaker0388@gmail.com>
2024-12-09 23:40:56 -0600

    update website-url

--- src/components/Tokens/TokenDetails/About.tsx
+++ src/components/Tokens/TokenDetails/About.tsx
@@ -96,7 +96,7 @@ export function AboutSection({ address, chainId, description, homepageUrl, twitt
 
   let displayHomePageUrl = homepageUrl;
   if (chainId == 96369) {
-    displayHomePageUrl = 'https://luxco.in';
+    displayHomePageUrl = 'https://lux.network';
   } else if (chainId == 200200) {
     displayHomePageUrl = 'https://zoo.network';
   }
